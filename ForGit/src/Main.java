d81946326fb29d03a9ea40503cf5e885e0d4b403
Yichou <fire1017@msn.com>
2017-02-17 14:53:20 +0800

    Pragmatic

--- ForGit/src/Main.java
+++ ForGit/src/Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Just For Test.");
 		System.out.println("altruistic!");
+		System.out.println("pragmatic!");
 	}
 
 }
