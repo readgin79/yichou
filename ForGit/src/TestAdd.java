81a848c1d370f0c05d516df17a21fa01b39bd8ad
readgin79 <ycl1017@gmail.com>
2017-02-18 15:47:02 +0800

    Update TestAdd.java

--- ForGit/src/TestAdd.java
+++ ForGit/src/TestAdd.java
@@ -3,6 +3,6 @@ public class TestAdd {
 	public static void main(String args[]){
 		System.out.println("xxx");
 		System.out.println("ssss");
-		System.out.println("new");
+		System.out.println("old");
 	}
 }
